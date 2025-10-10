fdc41d5939c075e7e0ad2a5559db2353aac29303
kanna Yepuri <lakshminarayanayepuri123@gmail.com>
2025-10-10 11:23:45 +0530

    Added GameBoard Componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="PLAYER 1" symbol="X"/>
           <Player initialName="PLAYER 2" symbol="O"/>
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   )
